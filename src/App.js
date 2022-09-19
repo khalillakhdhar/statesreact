dd26df4264a720eb0d562ffdfcea67e11a5293ee
khalil <khalillakhdharatc@gmail.com>
2022-09-19 13:47:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ class App extends React.Component
 {
   constructor()
   {
-    super();
+    // bloc d'initialisation
+    super(); // charger les paramétre par défault states et props
     this.state={
       nom:'Khalil',
       prenom:'lakhdhar',
       clicks:0
     }
+    this.increment=this.increment.bind(this);
+    this.decrement=this.decrement.bind(this);
+  }
+  increment()
+  {
+    // eslint-disable-next-line react/no-direct-mutation-state
+    let valn=this.state.clicks + 1;
+    this.setState({ clicks:valn  });
+  }
+  decrement()
+  {
+    // eslint-disable-next-line react/no-direct-mutation-state
+    let valn=this.state.clicks - 1;
+    this.setState({ clicks:valn  });
   }
     render()
     {
@@ -16,7 +31,9 @@ class App extends React.Component
         <div>
 Bienvenu {this.state.nom} {this.state.prenom}
 <br></br>
-{this.state.clicks}
+{this.state.clicks} clicks 
+<button onClick={this.increment}>+</button>&nbsp;
+<button onClick={this.decrement}>-</button>
         </div>
       )
     }
