92f90a6e2f6be81bbf0bf82eef2fc0ffbe9dad8a
khalil <khalillakhdharatc@gmail.com>
2022-09-19 13:29:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+class App extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state={
+      nom:'Khalil',
+      prenom:'lakhdhar'
+    }
+  }
+    render()
+    {
+      return(
+        <div>
+Bienvenu {this.state.nom} {this.state.prenom}
+        </div>
+      )
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  }
+
 
 export default App;
